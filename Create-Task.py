c5a3fb566032d9d8a081397c9ff131358f211c33
MikeBeehler <mikebeelz214@gmail.com>
2023-04-24 17:24:26 -0400

    Most Bugs Fixed?

--- Create-Task.py
+++ Create-Task.py
@@ -1,36 +1,20 @@
 #BlackJack 
 import random
 play_again = "Y"
-bet = 0 
+bet_for_game = 0 
 user_card_total = 0 
 dealer_card_total = 0
 round_counter = 0 
 user_win = False
 dealer_win = False
 repeat_game = "N"
+
 #Dictionary of all cards and their values
 card_values = {"2":2, "3":3, "4":4, "5":5, "6":6, "7":7, "8":8, "9":9, "10":10, "J":10, "Q":10, "K":10, "A":11}
 
-#Asking the user if they know how to play the game
-know_rules = input("Welcome to my take on the game of BlackJack! If you want to see my rules, please type 'Y'. If not, type 'N': ")
-
-while True:
-    
-    if know_rules.upper() == "Y" or know_rules.upper() == "YES":
-        #TYPE OUT RULES!
-        print ("This is a variation on the popular casino game known as Blackjack. The goal is to get as close to 21 as possible, without going over.")
-        print ("You will face off against the 'Dealer', who in this case is a computer.")
-        print ("You will make an initial bet, and then will draw (get) a random card, which has a value. ")
-        print ("After seeing this card, you can choose to double the amount of money you have bet, or to not put in additional money.")
-        print ("You can repeat this cycle until you don't want to recieve any more cards, until one of you gets 21, or until one of you goes over 21.")
-        print ("In the end, the person closest to 21 (but not over) wins. Winning takes your end bet and will multiply it by 3. I hope you enjoy!")
-        break
-    
-    elif know_rules.upper() == "N" or know_rules.upper() == "NO":
-        break
-        
-    else:
-        know_rules = str(input(("Invalid input. Please try again.")))
+print ("Welcome to 'One Card At A Time BlackJack'! You and the computer, aka the 'Dealer', will both be dealt one card after you have made a bet. ")
+print ("The goal of the game is to get as close to 21 as possible, without going over. You will make an ititial bet, and then can double it each round.")
+print ("I hope you have fun!")
 
 
 #Getting the User to enter a valid balance   
@@ -44,44 +28,44 @@ while True:
 
 #Now we have a balance, we can begin the game.
 
-def play_round (user_total, dealer_total):
+def play_round (bet_for_round, user_total, dealer_total):
     global repeat_game
-    global bet
+    global bet_for_game
     global balance
     global round_counter
     round_counter += 1
     print (f"Welcome to round {round_counter}.")
 
     #Gets a valid bet from the user
-    if bet == 0:
+    if bet_for_round != 0:
         while True:
             try:
-                bet = int(input(("Please enter your bet: ")))
-                while bet > balance:
-                    bet = int(input("Bet cannot be greater than current balance. Please enter a valid bet: "))
-                break
-            except:
-                print ("An error occurred! Please try again! (Make sure you are entering an integer)")
-        balance = balance - bet
-        
-    else:
-        while True:
-            try:
-                double_down = str(input(f"Please enter 'Y' if you would like to double your bet, and 'N' if you would just like to hit (Your current bet is {bet}): "))
+                double_down = str(input(f"Please enter 'Y' if you would like to double your bet, and 'N' if you would just like to hit (Your current bet is {bet_for_round}): "))
                 if double_down.upper() == "Y":
-                    if balance - bet < 0:
+                    if balance - bet_for_round < 0:
                         print ("Your balance is too low to double down. You will just hit")
                         break
                     else:
-                        balance = balance - bet
-                        bet = bet + bet
+                        balance = balance - bet_for_round
+                        bet_for_game = bet_for_round + bet_for_round
                         break
                 else:
                     break
             except:
                 print ("An error occurred! Please try again! (Make sure you are entering just the letter)")
             
-    
+    if bet_for_round == 0:
+        while True:
+            try:
+                bet_for_round = int(input(("Please enter your bet: ")))
+                while bet_for_round > balance:
+                    bet_for_round = int(input("Bet cannot be greater than current balance. Please enter a valid bet: "))
+                break
+            except:
+                print ("An error occurred! Please try again! (Make sure you are entering an integer)")
+        balance = balance - bet_for_round
+        bet_for_game = bet_for_round
+
     #Card Drawing and Adding to Total
     global play_again
     if play_again == "Y":
@@ -151,12 +135,12 @@ def play_round (user_total, dealer_total):
         user_win = True
     
     
-play_round (0, 0)
+play_round (0, 0, 0)
 
 play_again = str(input("Would you like to play another round? Please type 'Y' if you would like to continue, and 'N' if you would like to stand: "))
 
 while play_again == "Y" and user_card_total < 21 and user_win != True and dealer_win != True:
-    play_round (user_card_total, dealer_card_total)
+    play_round (bet_for_game, user_card_total, dealer_card_total)
     if user_card_total < 21 and user_win != True and dealer_win != True:
         play_again = str(input("Would you like to play another round? Please type 'Y' if you would like to continue, and 'N' if you would like to stand: "))
 
@@ -182,10 +166,10 @@ if dealer_card_total <=16 and user_card_total < 21:
     
 def check_win():
     #Check Win Conditions
-    global user_card_total, dealer_card_total, dealer_win, user_win, bet, winnings, balance, round_counter, play_again
+    global user_card_total, dealer_card_total, dealer_win, user_win, bet_for_game, winnings, balance, round_counter, play_again
     if user_card_total > dealer_card_total and dealer_win != True or user_win == True:
         print ("You win!")
-        winnings = bet * 3
+        winnings = bet_for_game * 3
         balance = balance + winnings
         print (f"Your winnings were: {winnings}")
     
@@ -196,7 +180,7 @@ def check_win():
     
     #Resetting the Variables for the Next Game
     round_counter = 0
-    bet = 0
+    bet_for_game = 0
     play_again = "Y"
     winnings = 0 
     user_win = False
@@ -225,12 +209,12 @@ repeat ()
 
 while repeat_game == "Y":
     
-    play_round (0, 0)
+    play_round (0, 0, 0)
 
     play_again = str(input("Would you like to play another round? Please type 'Y' if you would like to continue, and 'N' if you would like to stand: "))
 
     while play_again == "Y" and user_card_total < 21 and user_win != True and dealer_win != True:
-        play_round (user_card_total, dealer_card_total)
+        play_round (bet_for_game, user_card_total, dealer_card_total)
         if user_card_total < 21 and user_win != True and dealer_win != True:
             play_again = str(input("Would you like to play another round? Please type 'Y' if you would like to continue, and 'N' if you would like to stand: "))
     
